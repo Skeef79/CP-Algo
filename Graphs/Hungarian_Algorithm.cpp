6da815e65d585650a09b3fb32f84ec03c18c655d
Igor Subbotin <theskeef79@gmail.com>
2020-07-06 00:37:32 +0500

    Update Hungarian_Algorithm.cpp

--- Graphs/Hungarian_Algorithm.cpp
+++ Graphs/Hungarian_Algorithm.cpp
@@ -4,7 +4,8 @@ pair<ll, vector<int>> hungarianAlgorithm(vector<vector<ll>> a)
 	int m = a[0].size() - 1;
 
 	vector<int> pl(n + 1, 0), pr(n + 1, 0), mt(n + 1, 0), way(n + 1, 0);
-	for (int row = 1; row <= n; row++) {
+	for (int row = 1; row <= n; row++) 
+	{
 		mt[0] = row;
 		vector<int> minv(n + 1, INF);
 		vector<bool> used(n + 1, false);
@@ -13,7 +14,8 @@ pair<ll, vector<int>> hungarianAlgorithm(vector<vector<ll>> a)
 		{
 			used[col] = true;
 			int v = mt[col], delta = INF, nextcol;
-			for (int i = 1; i <= n; i++) {
+			for (int i = 1; i <= n; i++) 
+			{
 				if (used[i])
 					continue;
 				int x = a[v][i] - pl[v] - pr[i];
@@ -22,7 +24,8 @@ pair<ll, vector<int>> hungarianAlgorithm(vector<vector<ll>> a)
 				if (minv[i] < delta)
 					delta = minv[i], nextcol = i;
 			}
-			for (int i = 0; i <= n; i++) {
+			for (int i = 0; i <= n; i++) 
+			{
 				if (used[i])
 					pl[mt[i]] += delta, pr[i] -= delta;
 				else
